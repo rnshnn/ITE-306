4e3a695561b3e26e766760cb4e58ae108a952581
Rianne Shiane Sangbaan <115058859+rnshnn@users.noreply.github.com>
2022-10-19 14:51:48 +0800

    Add files via upload

--- sas20/script.py
+++ sas20/script.py
@@ -3,9 +3,9 @@ from flask import *
 app = Flask(__name__)
 
 
-@app.route('/table/<int:num>')
-def table(num):
-    return render_template('print-table.html', n=num)
+@app.route('/rianne')
+def message():
+    return render_template('message.html')
 
 
 if __name__ == '__main__':
